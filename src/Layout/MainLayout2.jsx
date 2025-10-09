7b9f811f53068086b3067d2e33c47ea6819c0cba
Suvro Biswas <shuvo.249963@gmail.com>
2025-10-09 22:10:55 +0600

    Fix Loading issue

--- src/Layout/MainLayout2.jsx
+++ src/Layout/MainLayout2.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
-import { Outlet, useLoaderData } from 'react-router';
+import { Outlet } from 'react-router';
 import Loader from '../Components/Loader';
+import { useLoading } from '../Hooks/useLoading';
 
 const MainLayout2 = () => {
-    const isLoading = useLoaderData()
+    const isLoading = useLoading()
     return (
         <div className='flex flex-col min-h-screen'>
             <Navbar></Navbar>
